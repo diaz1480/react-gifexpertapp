ca394098ed7402e8d6f86f93ca1113606956f9ea
Juan Camilo Diaz <diaz1480@gmail.com>
2020-10-18 20:19:11 -0500

    docs agregado

---
+++ docs/precache-manifest.a61303461f0ef19e50f8551870edd04e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8170d15116908773af42c7acc097166",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76bcd42db9132aecc344",
+    "url": "/static/css/main.2c5773ee.chunk.css"
+  },
+  {
+    "revision": "7e891c718646e7257925",
+    "url": "/static/js/2.bc4ddd0a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.bc4ddd0a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "76bcd42db9132aecc344",
+    "url": "/static/js/main.96b1ca6f.chunk.js"
+  },
+  {
+    "revision": "722a304d2cb62627ceb8",
+    "url": "/static/js/runtime-main.3cc8bee4.js"
+  }
+]);
